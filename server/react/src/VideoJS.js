2b3c0828b649a7e1f7bfd0d8a80ceb5a20bb1a17
Leon Handreke <leonh@ndreke.de>
2018-04-26 09:55:13 +0200

    Clappr WIP

--- server/react/src/VideoJS.js
+++ server/react/src/VideoJS.js
@@ -1,43 +1,50 @@
 import React from 'react';
 import videojs from 'video.js'
 import axios from 'axios'
-//import chromecast from 'videojs-chromecast'
-import videojshls from "videojs-contrib-hls"
-import videojsflash from "videojs-flash"
+import Clappr from 'clappr'
+import ChromecastPlugin from 'clappr-chromecast-plugin'
 
 class VideoPlayer extends React.Component {
 
   componentDidUpdate() {
-    this.player = videojs(this.videoNode)
-    this.player.src(this.props.sources)
-    this.player.play()
-    if(this.props.playtime != 0){
-	    this.player.currentTime(this.props.playtime)
-    }
+    //this.player.src(this.props.sources)
+    //this.player.play()
+    //if(this.props.playtime != 0){
+	  //  this.player.currentTime(this.props.playtime)
+    //}
+    this.player = new Clappr.Player({
+      parent: this.refs.player,
+      source: this.props.sources[0]['src'],
+      plugins: [ChromecastPlugin],
+      autoplay: true,
+      chromecast: {},
+      hlsjsConfig: {
+        enableWorker: true
+      }
+    })
   }
 
   componentDidMount() {
-    window.VIDEOJS_NO_DYNAMIC_STYLE = true
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-      console.log("videojs loaded")
-    });
-    this.player.on("timeupdate", (e) => {
-	    let playtime = e.target.player.currentTime()
-	    if((Math.round(playtime % 5)) === 0){
-		    axios.post(`${this.props.serverAddress}api/v1/state`, {playtime: Math.floor(playtime), filename: e.target.player.currentSource().name}).then(response => {
-			    console.log("Successful state update")
-		    }).catch(error => {
-			    console.log("Error pushing state", error);
-		    })
-	    }
-    })
+    //this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+    //  console.log("videojs loaded")
+    //});
+    //this.player.on("timeupdate", (e) => {
+	  //  let playtime = e.target.player.currentTime()
+	  //  if((Math.round(playtime % 5)) === 0){
+		//    axios.post(`${this.props.serverAddress}api/v1/state`, {playtime: Math.floor(playtime), filename: e.target.player.currentSource().name}).then(response => {
+		//	    console.log("Successful state update")
+		//    }).catch(error => {
+		//	    console.log("Error pushing state", error);
+		//    })
+	  //  }
+    //})
 
   }
 
   // destroy player on unmount
   componentWillUnmount() {
     if (this.player) {
-      this.player.dispose()
+      this.player.destroy()
     }
   }
 
@@ -47,8 +54,7 @@ class VideoPlayer extends React.Component {
   render() {
     return (
       <div>
-        <div data-vjs-player>
-          <video ref={ node => this.videoNode = node } className="video-js vjs-default-skin" controls></video>
+        <div ref="player">
         </div>
       </div>
     )
