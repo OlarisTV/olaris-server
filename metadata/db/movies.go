8a34cc8517e8183fe21bdf8686fc9c6340460dd3
Maran <maran@protonmail.com>
2018-06-06 11:26:10 +0200

    more refactor

---
+++ metadata/db/movies.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+type MovieItem struct {
+	gorm.Model
+	MediaItem
+	TmdbID        int
+	ReleaseDate   string
+	OriginalTitle string
+	ImdbID        string
+}
+
+func (self *MovieItem) String() string {
+	return fmt.Sprintf("Movie: %s\nYear: %d\nPath:%s", self.Title, self.Year, self.FilePath)
+}
+
+func FindAllMovies() (movies []MovieItem) {
+	ctx.Db.Where("tmdb_id != 0").Find(&movies)
+	return movies
+}
+
+func FindMoviesInLibrary(libraryID uint) (movies []MovieItem) {
+	ctx.Db.Where("library_id = ? AND tmdb_id != 0", libraryID).Find(&movies)
+
+	return movies
+}
---
+++ metadata/db/tv.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type TvSeries struct {
+	UUIDable
+	gorm.Model
+	BackdropPath string
+	PosterPath   string
+	Name         string
+	Overview     string
+	FirstAirDate string
+	FirstAirYear uint64
+	OriginalName string
+	Status       string
+	TmdbID       int
+	Type         string
+}
+
+type TvEpisode struct {
+	gorm.Model
+	MediaItem
+	Name       string
+	SeasonNum  string
+	EpisodeNum string
+	TvSeasonID uint
+	TmdbID     int
+	AirDate    string
+	StillPath  string
+	TvSeason   *TvSeason
+}
+
+func FindAllSeries() (series []TvSeries) {
+	ctx.Db.Where("tmdb_id != 0").Find(&series)
+	return series
+}
+func FindSeasonsForSeries(seriesID uint) (seasons []TvSeason) {
+	ctx.Db.Where("tv_series_id = ?", seriesID).Find(&seasons)
+	return seasons
+}
+func FindEpisodesForSeason(seasonID uint) (episodes []TvEpisode) {
+	ctx.Db.Where("tv_season_id = ?", seasonID).Find(&episodes)
+	return episodes
+}
+func FindEpisodesInLibrary(libraryID uint) (episodes []TvEpisode) {
+	ctx.Db.Where("library_id =?", libraryID).Find(&episodes)
+	return episodes
+}
