0a977b3023194984a292f43856318da05a9ad5f7
Leon Handreke <leonh@ndreke.de>
2018-09-23 12:22:30 +0200

    [streaming] Remove old demo player files
    Continuation of 51ed4fdd04766092aaf07874bca4860feffc8322 that removed
    the handler.

--- streaming/react/src/CheckCodecCompatibility.js
+++
@@ -1,6 +0,0 @@
-const videoElement = document.createElement('video');
-
-export function isCodecCompatible(codec) {
-  let response = videoElement.canPlayType("video/mp4; codecs=" + codec)
-  return response === "probably"
-}
