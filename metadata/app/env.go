10b056a375e2d0095d18b997e31911d7d216fdc9
Maran <maran@protonmail.com>
2018-10-01 16:11:39 +0200

    Force rescan when a file can't be found anymore

--- metadata/app/env.go
+++ metadata/app/env.go
@@ -130,8 +130,8 @@ loop:
 			env.Watcher.Close()
 			break loop
 		case event := <-env.Watcher.Events:
+			log.WithFields(log.Fields{"filename": event.Name, "event": event.Op}).Debugln("Got filesystem notification event.")
 			if managers.ValidFile(event.Name) {
-				log.WithFields(log.Fields{"filename": event.Name}).Debugln("Got filesystem notification for valid media file.")
 				if event.Op&fsnotify.Rename == fsnotify.Rename {
 					log.Debugln("File is renamed, forcing removed files scan.")
 					env.LibraryManager.CheckRemovedFiles() // Make this faster by only scanning the changed file
@@ -153,6 +153,11 @@ loop:
 						}
 					}
 				}
+			} else {
+				log.WithFields(log.Fields{"filename": event.Name, "event": event.Op}).Debugln("Got an error while trying to open file. Going to assume the file was removed.")
+				log.Debugln("File is removed, forcing removed files scan and removing fsnotify watch.")
+				env.Watcher.Remove(event.Name)
+				env.LibraryManager.CheckRemovedFiles() // Make this faster by only scanning the changed file
 			}
 		case err := <-env.Watcher.Errors:
 			log.Warnln("fsnotify watcher error:", err)
