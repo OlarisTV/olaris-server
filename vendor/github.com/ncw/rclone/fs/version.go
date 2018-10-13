2c67ee45405baa303d2ca296e5e10fd680d25c39
Leon Handreke <leonh@ndreke.de>
2018-10-13 11:32:17 +0200

    [streaming] Remove rclone dependency for now

--- vendor/github.com/ncw/rclone/fs/version.go
+++
@@ -1,4 +0,0 @@
-package fs
-
-// Version of rclone
-var Version = "v1.41"
--- vendor/github.com/ncw/rclone/fs/versioncheck.go
+++
@@ -1,7 +0,0 @@
-//+build !go1.7
-
-package fs
-
-// Upgrade to Go version 1.7 to compile rclone - latest stable go
-// compiler recommended.
-func init() { Go_version_1_7_required_for_compilation() }
