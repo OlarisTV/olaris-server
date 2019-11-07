OlarisTV/olaris-server

commit f7885cd3cc8c605987789a4ceb71496078962c27

Fix CI artifact generation

Makefile
@@ -50,7 +50,7 @@ build-olaris-react:
 	make generate
 
 .PHONY: build
-build: generate
+build:
 	GOOS=$(GOOS) GOARCH=$(GOARCH) $(GOBUILD) -o $(BIN_LOC)/$(IDENTIFIER) $(LDFLAGS) -v $(CMD_SERVER_PATH)
 
 .PHONY: build-local

.gitlab-ci.yml
@@ -19,6 +19,7 @@ before_script:
   - go get github.com/elazarl/go-bindata-assetfs/...
   - go get github.com/maxbrunsfeld/counterfeiter
   - make ready-ci
+  - make generate
 
 stages:
     - format
@@ -35,7 +36,7 @@ build:
     stage: build
     script:
       - make build-linux
-      - cp -r build CI_PROJECT_DIR/
+      - cp -r build $CI_PROJECT_DIR/
     artifacts:
       paths:
         - build/olaris-linux-386

commit f048d255e852d8c3aae9c1bf1761eb713779d918

[build] Make build depend on generate again
Otherwise the wrong ffmpeg binary gets included. This war broken by me
in f7885cd3cc8c605987789a4ceb71496078962c27.

Makefile
@@ -52,7 +52,7 @@ build-olaris-react:
 	make generate
 
 .PHONY: build
-build:
+build: generate
 	GOOS=$(GOOS) GOARCH=$(GOARCH) $(GOBUILD) -o $(BIN_LOC)/$(IDENTIFIER) $(LDFLAGS) -v $(CMD_SERVER_PATH)
 
 .PHONY: build-local