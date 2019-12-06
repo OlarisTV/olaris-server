6359336d204d6f8ed470d8d7c1bf078d12251e5f
Chris Lee <clee@mg8.org>
2019-12-05 23:39:05 -0700

    Actually use Lock() in locks.go
    Fixes #134, I think. (The crashes I saw took multiple hours to occur, but
    after looking at the code, it seems pretty clear why it was crashing,
    and I think this fix is correct.)

--- metadata/helpers/locks.go
+++ metadata/helpers/locks.go
@@ -2,20 +2,29 @@ package helpers
 
 import (
 	log "github.com/sirupsen/logrus"
+	"sync"
 )
 
+var mutex = &sync.Mutex{}
 var refreshMDLocks = make(map[string]bool)
 
 // WithLock keeps track of metaitems that are being refreshed based on an id or uuid and locks them so they can't be refreshed twice.
 func WithLock(fn func(), id string) {
 	log.WithFields(log.Fields{"lockID": id}).Debugln("Checking for lock.")
-	if refreshMDLocks[id] == false {
-		log.WithFields(log.Fields{"lockID": id}).Debugln("No lock.")
+	mutex.Lock()
+	if _, exists := refreshMDLocks[id]; !exists {
+		log.WithFields(log.Fields{"lockID": id}).Debugln("No lock found, locking...")
 		refreshMDLocks[id] = true
+		log.WithFields(log.Fields{"lockID": id}).Debugln("Locked.")
+
+		// No need to hold the mutex while fn() runs, so release it before we need it again
+		mutex.Unlock()
 		fn()
-		refreshMDLocks[id] = false
-		log.WithFields(log.Fields{"lockID": id}).Debugln("Lock released.")
+		mutex.Lock()
+		delete(refreshMDLocks, id)
 	} else {
 		log.WithFields(log.Fields{"lockID": id}).Warnln("Already had a lock, ignoring.")
 	}
+	mutex.Unlock()
+	log.WithFields(log.Fields{"lockID": id}).Debugln("Lock released.")
 }
