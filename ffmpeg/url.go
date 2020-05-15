efd53a231e2eb38b2b5ae422a8f448339dc39f19
Chris Lee <clee@mg8.org>
2020-05-15 01:03:25 -0600

    Remove FfmpegUrlPort hacks

--- ffmpeg/url.go
+++ ffmpeg/url.go
@@ -2,15 +2,12 @@ package ffmpeg
 
 import (
 	"fmt"
+	"net/url"
+
+	"github.com/spf13/viper"
 	"gitlab.com/olaris/olaris-server/filesystem"
 	"gitlab.com/olaris/olaris-server/metadata/auth"
-	"net/url"
 )
-
-// TODO(Leon Handreke): Figure out a better way than a package-global variable to
-// convey this info from the top-level command flag to ffmpeg. Or maybe a setter is enough?
-// Also this should go in some util package to build URLs
-var FfmpegUrlPort = 8080
 
 // NOTE(Leon Handreke): This doesn't really belong here. It doesn't really belong anywhere since it
 // really breaks the layering. I'm not really sure where to put this. Probably some URL building
@@ -20,7 +17,7 @@ func buildFfmpegUrlFromFileLocator(fileLocator filesystem.FileLocator) string {
 	case filesystem.BackendRclone:
 		jwt, _ := auth.CreateStreamingJWT(0, fileLocator.String())
 		return fmt.Sprintf("http://127.0.0.1:%d/olaris/s/files/jwt/%s",
-			FfmpegUrlPort, url.PathEscape(jwt))
+			viper.GetInt("server.port"), url.PathEscape(jwt))
 	case filesystem.BackendLocal:
 		return "file://" + fileLocator.Path
 	}
