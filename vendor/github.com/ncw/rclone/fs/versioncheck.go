7d9e1e9a8ba9b45a0255a277657947874020327c
Devin Buhl <devin.kray@gmail.com>
2020-05-18 12:50:02 -0400

    initial tidying

--- vendor/github.com/ncw/rclone/fs/versioncheck.go
+++
@@ -1,7 +0,0 @@
-//+build !go1.9
-
-package fs
-
-// Upgrade to Go version 1.9 to compile rclone - latest stable go
-// compiler recommended.
-func init() { Go_version_1_9_required_for_compilation() }
