4d172e2e45ea35f395bc7448210152e8feb0da13
Animazing <Animazing@protonmail.com>
2021-01-26 19:27:08 +0100

    Add simple version http handler

--- metadata/handler.go
+++ metadata/handler.go
@@ -5,8 +5,10 @@ package metadata
 import (
 	"github.com/gorilla/mux"
 	"github.com/graph-gophers/graphql-transport-ws/graphqlws"
+	"gitlab.com/olaris/olaris-server/helpers"
 	"gitlab.com/olaris/olaris-server/metadata/app"
 	"gitlab.com/olaris/olaris-server/metadata/resolvers"
+	"net/http"
 
 	"gitlab.com/olaris/olaris-server/metadata/auth"
 )
@@ -20,9 +22,15 @@ func RegisterRoutes(menv *app.MetadataContext, r *mux.Router) {
 
 	r.HandleFunc("/v1/auth", auth.UserHandler).Methods("POST")
 
+	r.HandleFunc("/v1/version", versionHandler).Methods("GET")
+
 	r.HandleFunc("/v1/user", auth.CreateUserHandler).Methods("POST")
 	r.HandleFunc("/v1/user/setup", auth.ReadyForSetup)
 
 	// TODO(Maran): This should be authenticated too.
 	r.HandleFunc("/images/{provider}/{size}/{id}", imageManager.HTTPHandler)
+}
+
+func versionHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte(helpers.Version))
 }
