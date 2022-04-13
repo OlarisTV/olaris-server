b53d331d6ecba4a7d972b20f23f5b8df41ff6751
Animazing <Animazing@protonmail.com>
2022-04-13 10:14:34 +0200

    Updates the folders() GraphQL query to return full paths when requested

--- metadata/resolvers/folders.go
+++ metadata/resolvers/folders.go
@@ -2,18 +2,23 @@ package resolvers
 
 import (
 	"context"
-	"gitlab.com/olaris/olaris-server/filesystem"
+	"path/filepath"
 	"sort"
+
+	log "github.com/sirupsen/logrus"
+	"gitlab.com/olaris/olaris-server/filesystem"
 )
 
 type folderArgs struct {
-	Path string
+	Path     string
+	FullPath bool
 }
 
 // Remotes returns all Folders in the given path, takes a FileLocator and returns all folders in the given folder
 func (r *Resolver) Folders(ctx context.Context, args *folderArgs) (folders []*string) {
 	err := ifAdmin(ctx)
 	if err != nil {
+		log.Error("Error:", err)
 		return folders
 	}
 
@@ -23,6 +28,7 @@ func (r *Resolver) Folders(ctx context.Context, args *folderArgs) (folders []*st
 	}
 
 	n, err := filesystem.GetNodeFromFileLocator(locator)
+
 	if err != nil {
 		return folders
 	}
@@ -35,6 +41,9 @@ func (r *Resolver) Folders(ctx context.Context, args *folderArgs) (folders []*st
 	sort.Strings(f)
 
 	for i := range f {
+		if args.FullPath {
+			f[i] = filepath.Join(locator.Path, f[i])
+		}
 		folders = append(folders, &f[i])
 	}
 
